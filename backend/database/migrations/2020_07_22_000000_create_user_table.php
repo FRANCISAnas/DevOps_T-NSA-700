eb93afef32d4132042dad840a5d11d1214be43bb
jonas haberkorn <jonas.haberkorn16@gmail.com>
2020-02-08 17:45:07 +0100

    Making huge changes, not tested yet

---
+++ backend/database/migrations/2020_07_22_000000_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateuSERTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email');
+            $table->timestamp('email_verified_at');
+            $table->string('password');
+            $table->string('remember_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
